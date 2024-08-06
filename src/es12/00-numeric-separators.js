a796e0cb3b3e195bb3fdec19a6a1bf0ec085540c
runinbk <runinbk@gmail.com>
2024-08-05 21:56:50 -0400

    ES12: numeric separators & replaceall

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,8 @@
+
+const value = 100000000000;
+const value1 = 100_000_000_000;     // no afecta en nada los separadores, y estos nos pueden servir para separar los digitos
+
+console.log(value);
+console.log(value1);
+
+
---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,7 @@
+
+const string = "JavaScript es un maravillos lenguaje de programacion";
+
+const replaceString = string.replace("JavaScript", "TypeScript");
+
+console.log(string);
+console.log(replaceString);
